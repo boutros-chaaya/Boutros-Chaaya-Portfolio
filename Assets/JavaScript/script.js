d9d337c2fb291a78949c6a2dd5a899188f616752
boutros-chaaya <121110787+boutros-chaaya@users.noreply.github.com>
2025-02-15 21:50:43 +0200

    Update script.js

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  window.onscroll = function () {
+    const scrollButton = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.style.display = "block"; // Show the button
+    } else {
+      scrollButton.style.display = "none"; // Hide the button
+    }
+  };
